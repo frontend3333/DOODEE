6c6723c76248de21b0083c8d794f5dfd7f8ddb66
ORG <47654977+ryeongee@users.noreply.github.com>
2021-10-09 17:49:42 +0900

    .

--- doodee/src/pages/Process.js
+++ doodee/src/pages/Process.js
@@ -2,16 +2,50 @@ import React from 'react';
 import './Process.css';
 const Process = () => {
     return (
-        <div>
-           process
-           <ul>
-               <li>aa</li>
-               <li>aa</li>
-               <li>aa</li>
-               <li>aa</li>
-
-           </ul>
-        </div>
+        <>
+            <link href="//db.onlinewebfonts.com/c/6dd2f2510b4a00a5461b2455928209c2?family=BrandonGrotW01-Light" rel="stylesheet" type="text/css"/>
+            <link href="//db.onlinewebfonts.com/c/39a2c7f346d5cfae7045aeb2fb50d9ad?family=LuloCleanW01-OneBold" rel="stylesheet" type="text/css"/>
+            <div className='process-container'>
+                <div className='grid-container'>
+                    <div className='first-grid-item'>
+                        <h1>
+                            <span>process</span>
+                        </h1>
+                    </div>
+                    <div className='second-grid-item'>
+                        <div className='interior-flex-container'>
+                            <div className='interior-flex-item'>
+                                <div className='home-interior'>
+                                    <img src='https://static.wixstatic.com/media/f1b974_156efcb102334840a2fd06fd4b00d9ec~mv2.jpg/v1/crop/x_359,y_0,w_1997,h_2000/fill/w_148,h_148,al_c,q_80,usm_0.66_1.00_0.01/449.webp' alt='no-img'></img>
+                                    <h2>주거공간</h2>
+                                </div>
+                            </div> 
+                            <div className='interior-flex-item'>
+                                <div className='commercial-interior'>
+                                    <img src='https://static.wixstatic.com/media/f1b974_91e07437552442d3a538be44d3febc31~mv2.jpg/v1/fill/w_148,h_148,al_c,q_80,usm_0.66_1.00_0.01/shutterstock_1065115037.webp' alt='no-img'></img>
+                                    <h2>상업공간</h2>
+                                </div>
+                            </div>                            
+                            <div className='interior-flex-item'>
+                                <div className='office-interior'>
+                                    <img src='https://static.wixstatic.com/media/f1b974_78166cee4dbf452197d9c55273d133c2~mv2.jpg/v1/crop/x_0,y_578,w_2188,h_2189/fill/w_148,h_148,al_c,q_80,usm_0.66_1.00_0.01/shutterstock_1223365972_%EC%9B%B9%EC%9A%A9.webp' alt='no-img'></img>
+                                    <h2>사무공간</h2>
+                                </div>
+                            </div>
+                            <div className='interior-flex-item'>
+                                <div className='styling'>
+                                    <img src='https://static.wixstatic.com/media/f1b974_3e4ac78ac0cd466e8d684a01d8d9bfe0~mv2.jpg/v1/crop/x_1404,y_0,w_4032,h_4032/fill/w_148,h_148,al_c,q_80,usm_0.66_1.00_0.01/1854.webp' alt='no-img'></img>
+                                    <h2>스타일링</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='third-grid-container'>
+                        <h2>construction process</h2>
+                    </div>
+                </div>
+            </div>
+        </>
     );  
 };
 
