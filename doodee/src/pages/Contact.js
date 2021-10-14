5e4dd67579a1aeff317d7314d74fb7686aed5731
minjai94 <77010869+minjai94@users.noreply.github.com>
2021-10-14 18:05:29 +0900

    Update Contact.js

--- doodee/src/pages/Contact.js
+++ doodee/src/pages/Contact.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import KakaoMap from '../components/KakaoMap';
 import './Contact.css';
 
-// adf
+// adfasd
 
 const Contact = () => {
     const [form, setForm] = useState({
